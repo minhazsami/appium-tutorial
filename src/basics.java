04d3f2a985439543926179b26bcc597b35d51e40
minhazsami <minhazsami@yahoo.com>
2022-02-10 19:00:12 +0600

    select selector, checkbox click, call index via Xpath

--- src/basics.java
+++ src/basics.java
@@ -19,6 +19,24 @@ public class basics extends Base {
 		 * 
 		 * 
 		 */
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		
+		//How to take ID Elements
+		
+		driver.findElementById("android:id/checkbox").click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//How to call index via xpath
+		
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+		
+		driver.findElementById("android:id/edit").click();
+		driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
+		driver.findElementById("android:id/button1").click();
 	}
 
 }
