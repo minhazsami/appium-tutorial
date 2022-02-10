1b4162a1c11d77c9e1acd4c7833190aca87ee683
minhazsami <minhazsami@yahoo.com>
2022-02-10 16:11:49 +0600

    added xpath sytex comment

--- src/basics.java
+++ src/basics.java
@@ -1,14 +1,24 @@
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class besics extends Base {
+public class basics extends Base {
 
 	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 
 		AndroidDriver<AndroidElement> driver = capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//xpath id className, androidUIautomator
+		/*
+		 * 	xpath syntax
+		 *	//tagName[@attribute='value']
+		 * 
+		 * 
+		 */
 	}
 
 }
