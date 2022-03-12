ed1112247765dc1b5dbea386a62263dd204c3e92
minhazsami <minhazsami@yahoo.com>
2022-03-12 09:53:37 +0600

    Created new class gesture and Learning TouchAction gesture

--- src/gestures.java
+++ src/gestures.java
@@ -1,9 +1,16 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
-public class gestures {
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 
-	public static void main(String[] args) {
+public class gestures extends Base {
+
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 
+		AndroidDriver<AndroidElement> driver = capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 }
